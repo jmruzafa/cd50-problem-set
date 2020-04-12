d7068bb876348790847ddf327ad2762b13dafc50
Javier Reyes <javier.reyes@bestsecret.com>
2020-04-12 22:10:00 +0200

    Week 2 problem set and others

--- week2-arrays/substitution.c
+++ week2-arrays/substitution.c
@@ -5,19 +5,32 @@
 #include <stdlib.h>
 
 //function definition 
-bool is_numeric(string str);
+bool is_valid_key(char *str);
+void error_message(int error_code);
+bool is_unique_characters_string(char *str);
 
 int main(int argc, string argv[])
 {
     //if user does not provide the "key" stop the program
-    if(argc < 2 || argc > 2 || !is_numeric(argv[1]))
+    if(argc < 2 || argc > 2)
     {
-        printf("Usage: ./caesar key\n");
+        error_message(1);
         return 1;
     }
+    else if (strlen(argv[1]) < 26 || !is_valid_key(argv[1]))
+    {
+        error_message(2);
+        return 1;
+    }
+    else if (!is_unique_characters_string(argv[1]))
+    {
+        error_message(3);
+        return 1;
+    }
+
     
     //get key to cypher
-    int rotation = (int) strtol(argv[1], NULL, 10);
+    char *key = argv[1]; 
     
     //ask the user for the text that will be parsed
     string plaintext = get_string("plaintext: ");
@@ -28,9 +41,8 @@ int main(int argc, string argv[])
 
 
     //Checks for the number of letters
-    int ascii_code = 0;
-    int new_code = 0;
-
+    int ascii_code = 0, new_code = 0, position = 0;
+    
     for (int i = 0, n = len; i < n; i++)
     {
         //Checks if a character is an alphanumeric character
@@ -40,9 +52,15 @@ int main(int argc, string argv[])
             int base = isupper(plaintext[i]) ? 65 : 97;
             //get actual ascii code
             ascii_code = plaintext[i];
-            //get new ascii code (if the rotation goes beyond the last letter (Z or z) it returns back to A or a
-            new_code = (((ascii_code - base) + rotation) % 26) + base;
-            ciphertext[i] = new_code;
+
+            //look for the relative position in the alphabet
+            position = ascii_code - base;
+
+            //letter from our keypass phrase
+            char substitute = key[position];
+            //get new ascii code remaining capitalized letters
+            ciphertext[i] = isupper(plaintext[i]) ? toupper(substitute) : tolower(substitute);
+            
         }
         else
         {
@@ -53,15 +71,52 @@ int main(int argc, string argv[])
     printf("ciphertext: %s\n", ciphertext);
 }
 
-bool is_numeric(string str) 
+bool is_valid_key(char *str)
 {
-    // loop Through each character in the string
+ // loop Through each character in the string
     for(int i = 0, n =  strlen(str); i < n; i++)
     {
-        if(!isdigit(str[i])) // Check if a single character "x" its a digit
+        if(!isalpha(str[i])) // Check if a single character "x" its a letter
         {
             return false;  // if its not return false
         } 
     }
     return true; // else return true
+}
+
+bool is_unique_characters_string(char *str)
+{
+    int len = strlen(str);
+     // If at any time we encounter 2 
+    // same characters, return false 
+    for (int i = 0; i < len - 1; i++) { 
+        for (int j = i + 1; j < len; j++) { 
+            if (str[i] == str[j]) { 
+                return false; 
+            } 
+        } 
+    } 
+    //no duplication found 
+    return true;
+}
+
+void error_message(int code_error)
+{
+    string message = "";
+    switch (code_error)
+    {
+    case 1:
+        message = "Usage: ./substitution key";
+        break;
+    case 2:
+        message = "Key must contain 26 characters.";
+        break; 
+    case 3:
+        message = "Key must not contain repeated characters";
+        break;
+    default:
+        message = "Something went wrong. Please, try again";
+        break; 
+    }
+    printf("%s\n", message);  
 }
