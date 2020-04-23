812ef20a1170e9d6db2df35a1da67d332a10eb9f
Javier Reyes <javier.reyes@bestsecret.com>
2020-04-23 21:34:29 +0200

    binary search fixed (but no recursion)

--- algorithms/binarysearch.c
+++ algorithms/binarysearch.c
@@ -2,55 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
+int print_array(int array[], int size);
+int binary_search(int list[], int size, int key);
+
+clock_t c1,c2;
 int main(void)
 {
     int n;
     int list[] = {1,2,4,5,6,7,9,12};
     int found = -1;
     int pos = 0;
-    clock_t c1 = clock(); 
-    clock_t c2 = clock();
+    
 
     printf("Type your number: ");
     scanf("%d", &n);
     
     int size = sizeof list / sizeof list[0];
-    int mid = size % 2;
+    print_array(list, size);
     
     //start timer
     clock_t c0 = clock();
+    found = binary_search(list, size, n);
 
-    if (list[mid] == n)
-    {
-        found = 0;
-    }
-    else if (n < list[mid])
-    {
-        for (int i = 0; i < mid; i++)
-        {   
-            if (n == list[i])
-            {
-                //stop timer
-                c1 = clock();
-                found = 0;
-            }
-        }
-    }
-    else if (n > list[mid])
-    {
-        for (int i = mid; i < size; i++)
-        {    
-            if (n == list[i])
-            {
-                //stop timer
-                c1 = clock();
-                found = 0;
-            }
-        }
-    }
-    
-    printf("\n");
-    
     c2 = clock();
     double found_diff_ms = (c1 - c0) * 1000. / CLOCKS_PER_SEC;
     double total_diff_ms = (c2 - c0) * 1000. / CLOCKS_PER_SEC;
@@ -64,4 +37,43 @@ int main(void)
         printf("Your number was not found!\n");
     }
     printf("Search took in total: %f ms\n", total_diff_ms);
+}
+
+int binary_search(int list[], int size, int key)
+{
+    int start = 0;
+    //last index of the array
+    int end = size - 1;
+
+    //loop while start is still lower that end point
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        printf("start: %i, end: %i, mid: %i. Value: %i\n", start, end, mid, list[mid]);
+        if (list[mid] == key)
+        {
+            c1 = clock();
+            return 0;
+        }
+        else if (key < list[mid])
+        {
+            //shorten the range and start again (recursive candidate)
+            end = mid - 1; 
+        }
+        else
+        {
+            //look for the value on the other side 
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
+int print_array(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        i < size-1 ? printf("%i, ", array[i]) : printf("%i", array[i]);
+    }
+    printf("\n");
+    return 0;
 }
