14745637c004024ca028a7e1d175db81a02758c3
Javier M. Ruzafa <46709710+jmruzafa@users.noreply.github.com>
2023-11-25 18:02:09 +0100

    Update population.c

--- pset1/population.c
+++ pset1/population.c
@@ -1,24 +1,37 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     // Prompt for starting population size
     int startSize;
-    do {
+    do
+    {
         startSize = get_int("Enter starting population size (must be >= 9): ");
     } while (startSize < 9);
 
     // Prompt for ending population size
     int endSize;
-    do {
-        end_size = get_int("Enter ending population size (must be >= starting size): ");
+    do
+    {
+        endSize = get_int("Enter ending population size (must be >= starting size): ");
     } while (endSize < startSize);
 
     // Calculate the number of years required
     int years = 0;
-    int currentSize = endSize;
-    while (currentSize < endSize) {
-        currentSize = current_size + (currentSize / 3) - (currentSize / 4);
+    int currentSize = startSize;
+    while (currentSize < endSize)
+    {
+        int newLlamas = currentSize / 3;
+        int passedAway = currentSize / 4;
+
+        // Ensure there's no fractional llamas
+        if (newLlamas - passedAway < 1)
+        {
+            newLlamas = 1;
+        }
+
+        currentSize = currentSize + newLlamas - passedAway;
         years++;
     }
 
