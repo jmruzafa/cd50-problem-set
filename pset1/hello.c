7ecd8da016ad6e62e6083e39fc09063d3d337d14
Javier M. Ruzafa <46709710+jmruzafa@users.noreply.github.com>
2023-11-27 21:56:25 +0100

    Update hello.c

--- pset1/hello.c
+++ pset1/hello.c
@@ -1,7 +1,6 @@
-#include <cs50.h>
 #include <stdio.h>
-
-int main(void)
-{
-    printf("hello, world!\n");
+#include <cs50.h>
+int main(void){
+    string s = get_string("What's your name: ");
+    printf("hello, %s", s);
 }
